22738d4f998dbe637cb31c0e4a80e2835ffd8bea
bruak <buraksoykan222@hotmail.com>
2023-07-12 18:11:01 +0300

    42

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,27 +6,24 @@
 /*   By: bsoykan <bsoykan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 14:36:50 by bsoykan           #+#    #+#             */
-/*   Updated: 2023/07/10 13:15:26 by bsoykan          ###   ########.fr       */
+/*   Updated: 2023/07/12 17:07:16 by bsoykan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize)
 {
 	size_t	i;
 
+	if (dstsize == 0)
+		return (ft_strlen(src));
 	i = 0;
-	if (size > 0)
+	while (src[i] && i < dstsize - 1)
 	{
-		while (src[i] && i < (size - 1))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
-	}
-	while (src[i])
+		dest[i] = src[i];
 		i++;
-	return (i);
+	}
+	dest[i] = 0;
+	return (ft_strlen(src));
 }
