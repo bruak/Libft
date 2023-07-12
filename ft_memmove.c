d629cf9e07bc9c94bceeab970e93a214c06f30f2
bruak <buraksoykan222@hotmail.com>
2023-07-12 14:33:33 +0300

    42

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,46 +6,32 @@
 /*   By: bsoykan <bsoykan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/09 14:38:03 by bsoykan           #+#    #+#             */
-/*   Updated: 2023/07/10 15:00:52 by bsoykan          ###   ########.fr       */
+/*   Updated: 2023/07/12 14:15:48 by bsoykan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	copy(int start, int end, char *dst, const char *src)
-{
-	int	i;
-	int	incr;
-
-	if (start == 0)
-		incr = 1;
-	else
-		incr = -1;
-	i = start;
-	while (i != end)
-	{
-		dst[i] = src[i];
-		i += incr;
-	}
-}
-
-void	directional_cpy(int forward, int len, char *dst, const char *src)
-{
-	if (forward)
-		copy(0, len + 1, dst, src);
-	else
-		copy(len, -1, dst, src);
-}
-
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char		*dest;
 	const char	*source;
+	int			i;
 
 	if (!dst && !src)
 		return ((void *) dst);
 	dest = dst;
 	source = src;
-	directional_cpy(dest < source, (int) len - 1, dest, source);
+	if (dest < source)
+	{
+		ft_memcpy(dest, source, len);
+		return (dst);
+	}
+	i = len - 1;
+	while (i >= 0)
+	{
+		dest[i] = source[i];
+		i--;
+	}
 	return (dst);
 }
