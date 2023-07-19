61357ed11e6698fd70aace2b011acc527e78fb6d
Burak Soykan <32242237+bruak@users.noreply.github.com>
2023-07-19 15:36:02 +0300

    Update ft_strlcat.c
    Fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,14 +12,14 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	i;
 	size_t	dst_len;
 	size_t	src_len;
 
 	i = 0;
-	if (size == '\0')
+	if (dsrsize == '\0')
 	{
 		return (ft_strlen(src));
 	}
@@ -27,13 +27,13 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	src_len = ft_strlen(src);
 	if (!size)
 		return (src_len);
-	while (src[i] && dst_len + i < size - 1)
+	while (src[i] && dst_len + i < dstsize - 1)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
 	}
 	dst[dst_len + i] = '\0';
-	if (dst_len > size)
-		return (src_len + size);
+	if (dst_len > dstsize)
+		return (src_len + dstsize);
 	return (src_len + dst_len);
 }
